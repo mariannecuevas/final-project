4a87fe4e13fca357d7bafacb6e886a76a6640e52
Marianne Cuevas <mariannecuevas97@gmail.com>
2025-02-12 04:59:15 +0000

    New Intial Commit

--- server/server.ts
+++ server/server.ts
@@ -7,9 +7,9 @@ import cors from 'cors';
 import argon2 from 'argon2';
 import jwt from 'jsonwebtoken';
 
-// const connectionString =
-//   process.env.DATABASE_URL ||
-//   `postgresql://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}:${process.env.RDS_PORT}/${process.env.RDS_DB_NAME}`;
+const connectionString =
+  process.env.DATABASE_URL ||
+  `postgresql://${process.env.RDS_USERNAME}:${process.env.RDS_PASSWORD}@${process.env.RDS_HOSTNAME}:${process.env.RDS_PORT}/${process.env.RDS_DB_NAME}`;
 
   const db = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
